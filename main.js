27c082dd95a09b9527dea03d26a70b8a84c076c2
ErnazarGeekTech <2017smaiferloke@gmail.com>
2023-02-03 18:17:33 +0300

    DZ-4

---
+++ main.js
@@ -0,0 +1,20 @@
+let btn = document.querySelectorAll('.button_one');
+let btnn = document.querySelectorAll('.button_two');
+let nums = document.querySelector('.num')
+let num = 0;
+
+for (let elem of btn) {
+    elem.addEventListener('click', (e) => {
+        num++
+        nums.innerHTML= num
+    })
+}
+for (let elem of btnn) {
+    elem.addEventListener('click', (e) => {
+        num--
+        nums.innerHTML= num
+    })
+}
+  
+
+
